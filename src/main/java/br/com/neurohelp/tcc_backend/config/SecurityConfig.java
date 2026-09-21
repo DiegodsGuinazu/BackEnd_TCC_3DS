01b7617f77b9d185b41d84eb0e55335ca2b00885
Diego dos Santos <diegodsguinazu@gmail.com>
2026-09-21 10:20:18 -0300

    fix: libera rotas de cadastro no SecurityConfigurations e habilita CORS

--- src/main/java/br/com/neurohelp/tcc_backend/config/SecurityConfig.java
+++ src/main/java/br/com/neurohelp/tcc_backend/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -69,5 +71,15 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://neuro-help-psi.vercel.app")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
+        }
     }
 }
