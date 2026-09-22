4989728d354e7e6e7c80ee65d35fa03b3021174a
Diego dos Santos <diegodsguinazu@gmail.com>
2026-09-22 12:01:37 -0300

    Adicionado scanBasePackages na classe principal

--- src/main/java/br/com/neurohelp/tcc_backend/TccBackendApplication.java
+++ src/main/java/br/com/neurohelp/tcc_backend/TccBackendApplication.java
@@ -3,9 +3,11 @@ package br.com.neurohelp.tcc_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.neurohelp.tcc_backend")
 @EntityScan("br.com.neurohelp.tcc_backend")
+@EnableJpaRepositories("br.com.neurohelp.tcc_backend")
 public class TccBackendApplication {
 
 	public static void main(String[] args) {
